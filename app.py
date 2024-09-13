8fb45b2626e7952bb62e09527e724f02804b1559
Fherlg <falg.0711@gmail.com>
2024-09-12 21:25:06 -0600

    crazyyy

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import flet as ft
+from views.home import HomeView
 
 def main(page: ft.Page):
-    page.add(ft.Text(value="Hello World!"))
+    page.title= "Mi app"
+    page.go("/home")
+
+    #Rutas para las vistas
+    def route_change(route):
+        page.views.clear()
+        if page.route  == "/home":
+            page.views.append(HomeView(page))
+        page.update()
+    
+    page.on_route_change = route_change
 
 ft.app(target=main)
--- views/home.py
+++ views/home.py
@@ -1,1 +1,10 @@
 import flet as ft
+
+def HomeView(page: ft.Page):
+    return ft.View(
+        "/home",
+        controls=[
+            ft.Text("Bienvenido a la pagina principal"),
+            ft.ElevatedButton(text="Click me", on_click=lambda e: print("Clicked"))
+        ]
+    )
